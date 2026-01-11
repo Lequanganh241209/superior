7b930edb736aadfd6ca9bfa23bc4c002668af573
unknown <TONLQ@THO-TONLQ-7390.tw.kbnn.vn>
2026-01-11 23:41:18 +0700

    Force Vercel Build V2.8 (Final Link Test)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ function Hero() {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
               </span>
-              <span>SYSTEM_READY_V2.7_VERCEL_KICKSTART</span>
+              <span>SYSTEM_READY_V2.8_VERCEL_FINAL_LINK</span>
             </div>
 
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter text-white leading-[1.1]">
